bba18b97857b9cda44e93400b675ae6eaf77a493
Will <will@w-p.dev>
2024-02-25 21:45:02 +0000

    migrate to albinobroccoli domain

--- website/registerSW.js
+++ website/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/path-blocks-public/sw.js', { scope: '/path-blocks-public/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
